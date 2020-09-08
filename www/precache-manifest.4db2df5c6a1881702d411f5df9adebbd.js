4a86d526d98d5107b08803b1bae79ea3a834781f
Joshua <joshua.richardson@myunidays.com>
2020-09-08 09:31:04 +0100

    Build scripts and cross platform support

---
+++ www/precache-manifest.4db2df5c6a1881702d411f5df9adebbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08791d0568833e61aec2127b3d5a773a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2e1138df57f6466a1c7",
+    "url": "./static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "a6c85d74769d4a5cea43",
+    "url": "./static/js/2.16866802.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.16866802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e1138df57f6466a1c7",
+    "url": "./static/js/main.5615d57f.chunk.js"
+  },
+  {
+    "revision": "1b5c148b9b0327f3781e",
+    "url": "./static/js/runtime-main.df6ee5cc.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
