23b4f0a7a6290772c43ca5a66d3e8ae7368386dc
Joshua <joshua.richardson@myunidays.com>
2020-09-08 09:31:04 +0100

    Added script to build in correct order

---
+++ scripts/prebuild.js
@@ -0,0 +1,67 @@
+// Execute the react build process
+// Rename the build folder to www as cordova uses the www folder for everything
+
+const path = require('path');
+const { exec } = require('child_process');
+const fs = require('fs');
+const rimraf = require('rimraf');
+const { resolve } = require('path');
+
+function renameOutputFolder(buildFolderPath, outputFolderPath) {
+    return new Promise((resolve, reject) => {
+        fs.rename(buildFolderPath, outputFolderPath, (err) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve('Successfully built!');
+            }
+        });
+    });
+}
+
+function execPostReactBuild(buildFolderPath, outputFolderPath) {
+    return new Promise((resolve, reject) => {
+        if(fs.existsSync(buildFolderPath)) {
+            if(fs.existsSync(outputFolderPath)) {
+                rimraf(outputFolderPath, (err) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    renameOutputFolder(buildFolderPath, outputFolderPath)
+                        .then(val => resolve(val))
+                        .catch(e => reject(e));
+                });
+            } else {
+                renameOutputFolder(buildFolderPath, outputFolderPath)
+                    .then(val => resolve(val))
+                    .catch( e => reject(e));
+            };
+        } else {
+            reject(new Error('build folder does not exist'));
+        }
+    });
+}
+
+module.exports = () => {
+    const projectPath = path.resolve(process.cwd(), './node_modules/.bin/react-scripts');
+    return new Promise((resolve, reject) => {
+        exec(`${projectPath} build`,
+        (error) => {
+            if(error) {
+                console.error(error);
+                reject(error)
+                return;
+            }
+            execPostReactBuild(path.resolve(__dirname, '../build/'), path.join(__dirname, '../www/'))
+                .then((s) => {
+                    console.log(s);
+                    resolve(s);
+                })
+                .catch((e) => {
+                    console.error(e);
+                    reject(e);
+                });
+        });
+    });
+}
